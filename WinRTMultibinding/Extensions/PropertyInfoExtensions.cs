f367a381bc70d25e3afab930379ff595597a14cb
Verbon <verbon@outlook.com>
2016-01-16 18:48:39 +0300

    CanRead/CanWrite extension methods now also check if property if private.

--- WinRTMultibinding/Extensions/PropertyInfoExtensions.cs
+++ WinRTMultibinding/Extensions/PropertyInfoExtensions.cs
@@ -5,9 +5,9 @@ namespace WinRTMultibinding.Extensions
     internal static class PropertyInfoExtensions
     {
         public static bool CanRead(this PropertyInfo propertyInfo)
-            => propertyInfo.CanRead && !propertyInfo.GetMethod.IsFamily;
+            => propertyInfo.CanRead && !propertyInfo.GetMethod.IsFamily && !propertyInfo.GetMethod.IsPrivate;
 
         public static bool CanWrite(this PropertyInfo propertyInfo)
-            => propertyInfo.CanWrite && !propertyInfo.SetMethod.IsFamily;
+            => propertyInfo.CanWrite && !propertyInfo.SetMethod.IsFamily && !propertyInfo.SetMethod.IsPrivate;
     }
 }
